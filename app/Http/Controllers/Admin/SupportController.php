7819adb2af3a4eb63acb9ef712ace26c9e6964cd
KaikeRochaDev <kaikerochadev@outlook.com>
2023-12-26 17:39:54 -0300

    refactor: use dto pattern

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTO\CreateSupportDTO;
+use App\DTO\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
@@ -34,10 +36,10 @@ class SupportController extends Controller
     }
 
     public function store(StoreUpdateSupport $request, Support $support){
-        $data = $request->validated();
-        $data['status'] = 'a';
 
-        $support = $support->create($data);
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request)
+        );
         
         return redirect()->route('supports.index');
     }
@@ -51,11 +53,14 @@ class SupportController extends Controller
     }
 
     public function update(StoreUpdateSupport $request, Support $support, string $id) {
-        if(!$support = $support->find($id)) {
+
+        $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request)
+        );
+
+        if (!$support) {
             return back();
         }
-
-        $support->update($request->validated());
 
         return redirect()->route('supports.index');
     }
