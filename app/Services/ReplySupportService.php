a4ce56de61499120f1536262aca2e67ea5613297
kaikerochadev <kaikerochadev@gmail.com>
2024-03-28 19:45:10 -0300

    feat: define relationships

---
+++ app/Services/ReplySupportService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+use App\DTO\Replies\CreateReplyDTO;
+use stdClass;
+
+class ReplySupportService {
+
+    public function getAllBySupportId(string $supportId): array {
+        return [];
+    }
+
+    public function createNew(CreateReplyDTO $dto): stdClass {
+        throw new \Exception('not implemented');
+    }
+}
