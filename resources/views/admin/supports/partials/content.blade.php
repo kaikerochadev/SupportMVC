1cf6fe8c2e6094d89631c0ba660992626a8466bb
kaikerochadev <kaikerochadev@gmail.com>
2024-04-04 19:43:03 -0300

    feat: use gate owner

--- resources/views/admin/supports/partials/content.blade.php
+++ resources/views/admin/supports/partials/content.blade.php
@@ -47,9 +47,11 @@
                             </td>
 
                             <td class="px-4 py-2 text-sm whitespace-nowrap flex">
-                                <a href="{{ route('supports.edit', $support->id) }}" class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg">
-                                    <p style="color: #D3D3D3">Editar</p>
-                                </a>
+                                @can('owner', $support->user_id)
+                                    <a href="{{ route('supports.edit', $support->id) }}" class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg">
+                                        <p style="color: #D3D3D3">Editar</p>
+                                    </a>
+                                @endcan
                                 <a href="{{ route('replies.index', $support->id) }}" class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg">
                                     <p class="text-emerald-500 gap-x-2">Ver detalhes</p>
                                 </a>
